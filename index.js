c20b884a32746f0da8891f820dd0b6350c3ad14d
Nathan Dunn <nathan.odin@gmail.com>
2018-02-16 12:11:58 -0500

    Finished layout, added dependencies, config webpack
    -(feature) Finished layout
    -(dependencies) Added jQuery, and webpack + plugins
    -(config) Configured Webpack

---
+++ index.js
@@ -0,0 +1,5 @@
+// Require Javascript files
+
+require('./src/js/app.js')
+
+require('./src/scss/styles.scss')
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    }
+};
